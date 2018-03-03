c3d37aa48953df5007c09a47c0d3e160d9d61aa6
newfable <newfable@github.com>
2018-03-03 23:33:36 +0800

    1

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wp1');
+define('DB_NAME', 'wordpress');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', 'newfable');
 
 /** MySQL数据库密码 */
 define('DB_PASSWORD', '84016626');
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '172.30.179.103');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8mb4');
